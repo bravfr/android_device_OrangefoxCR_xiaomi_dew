0415aa34af557936ea01cc5ebe76d9a202b4ebff
Demon000 <demonsingur@gmail.com>
2019-07-21 14:02:26 +0200

    dew: Define security patch level
    Can be found by checking the value of ro.vendor.build.security_patch
    on stock.
    
    Change-Id: I1d4a8bb4a5b6fdb4d1213fab0ea5e19eb7904ff7
    Signed-off-by: Alvyrion <alvyrion0@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -123,5 +123,9 @@ BOARD_USES_METADATA_PARTITION := true
 # Platform
 TARGET_BOARD_PLATFORM := mt6768
 
+# SPL
+BOOT_SECURITY_PATCH := 2026-02-01
+VENDOR_SECURITY_PATCH := 2026-02-01
+
 # Inherit the proprietary files
 include vendor/xiaomi/dew/BoardConfigVendor.mk
