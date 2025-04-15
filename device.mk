e8fd6257219a224c6b0209ae6909f0a5496cd56f
atharvnegi <atharvnegi2951@gmail.com>
2025-04-15 20:07:24 +0530

    dew: Include hardware/mediatek in Soong Namespaces
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -88,7 +88,8 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(DEVICE_PATH)
+    $(DEVICE_PATH) \
+    hardware/mediatek
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/dew/dew-vendor.mk)
