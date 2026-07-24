e29f42f5ba762b34b404d8e6bc8e11d43afcbf8b
bravfr <142199089+bravfr@users.noreply.github.com>
2026-07-24 17:31:52 -0500

    Update fox_dew.mk

--- fox_dew.mk
+++ fox_dew.mk
@@ -1,8 +1,3 @@
-# Heredar configuraciones base de la arquitectura del procesador
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-
-# Heredar especificaciones del motor base de OrangeFox
 $(call inherit-product, vendor/otfox/config/common.mk)
 
 # Identidad estricta del Redmi 15C
