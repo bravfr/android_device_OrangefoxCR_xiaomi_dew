b3ddae5a9471f78048cd4c5a245ec0b25a2441a9
bravfr <142199089+bravfr@users.noreply.github.com>
2026-07-24 21:56:38 -0500

    Update fox_dew.mk

--- fox_dew.mk
+++ fox_dew.mk
@@ -1,4 +1,10 @@
-# Identidad estricta del Redmi 15C
+# Heredar configuración base
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Heredar especificaciones de OrangeFox
+$(call inherit-product, vendor/otfox/config/common.mk)
+
+# Identidad del dispositivo
 PRODUCT_DEVICE := dew
 PRODUCT_NAME := fox_dew
 PRODUCT_BRAND := Xiaomi
