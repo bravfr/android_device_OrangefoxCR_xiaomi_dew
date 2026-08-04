9cc91ff8b9308723608eb4cbe466e4e987d5610a
bravfr <142199089+bravfr@users.noreply.github.com>
2026-08-03 21:08:55 -0500

    Update fox_dew.mk

--- fox_dew.mk
+++ fox_dew.mk
@@ -1,6 +1,10 @@
 # OrangeFox device configuration
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/xiaomi/dew/device.mk)
 
 PRODUCT_DEVICE := dew
 PRODUCT_NAME := fox_dew
